2070625179684dfa2cc1345f581c471ffba5ab90
magnuskoudahl <magnuskoudahl@gmail.com>
2022-07-12 16:34:34 +0200

    More experiments. There's numerical differences between FL and ReactiveMP implementations

--- src/categorical.jl
+++ src/categorical.jl
@@ -10,7 +10,7 @@ struct GFECategorical end
 end
 
 # We don't want log(0) to happen
-safelog(x) = log(x +eps())
+safelog(x) = log(x + eps())
 
 #note, why is it m_in and not q_in???
 @rule GFECategorical(:in, Marginalisation) (q_out::PointMass,q_in::DiscreteNonParametric, q_A::PointMass) = begin
