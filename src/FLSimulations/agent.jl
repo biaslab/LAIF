446cd5c3ebd42de53ad8e01de9c47e1e8a005f9c
Thijs van de Laar <thijsvdlaar@hotmail.com>
2022-07-14 16:42:21 +0200

    include MF planning

--- src/FLSimulations/agent.jl
+++ src/FLSimulations/agent.jl
@@ -1,10 +1,5 @@
-using ForneyLab: softmax #, unsafeMean, unsafeMode
-
 function evaluatePolicies(A, B, C, D)
-    C_t = [C, C]
-    # C_t = [ones(16)/16, C]
-
-    n_its = 20
+    n_its = 10
 
     # Evaluate all policies
     F = zeros(4,4)
@@ -12,14 +7,14 @@ function evaluatePolicies(A, B, C, D)
         for j = 1:4  # Second move
             data = Dict(:u       => [B[i], B[j]],
                         :A       => A,
-                        :C       => C_t,
+                        :C       => [C, C],
                         :D_t_min => D)
 
             marginals = Dict{Symbol, ProbabilityDistribution}(
                 :x_1 => ProbabilityDistribution(Univariate, Categorical, p=ones(8)./8),
                 :x_2 => ProbabilityDistribution(Univariate, Categorical, p=ones(8)./8))
         
-            for i=1:n_its
+            for k=1:n_its
                 step!(data, marginals)
             end
         
--- src/FLSimulations/update_rules/GFECategorical.jl
+++ src/FLSimulations/update_rules/GFECategorical.jl
@@ -1,8 +1,8 @@
-import ForneyLab: collectSumProductNodeInbounds
+import ForneyLab: collectSumProductNodeInbounds, collectNaiveVariationalNodeInbounds
 
 using ForneyLab: isClamped, assembleClamp!
 
-export ruleVBGFECategoricalOut
+export ruleVBGFECategoricalOut, ruleVBGFECategoricalOut
 
 @sumProductRule(:node_type     => GFECategorical,
                 :outbound_type => Message{Categorical},
@@ -12,7 +12,6 @@ export ruleVBGFECategoricalOut
 function ruleSPGFECategoricalOutDPP(marg_out::Distribution{Univariate, Categorical}, 
                                     msg_A::Message{PointMass, MatrixVariate},
                                     msg_c::Message{PointMass, Multivariate})
-
     s = marg_out.params[:p]
     A = msg_A.dist.params[:m]
     c = msg_c.dist.params[:m]
@@ -39,6 +38,42 @@ function collectSumProductNodeInbounds(::GFECategorical, entry::ScheduleEntry)
         else
             # Collect message from previous result
             push!(inbounds, interface_to_schedule_entry[inbound_interface])
+        end
+    end
+
+    return inbounds
+end
+
+@naiveVariationalRule(:node_type     => GFECategorical,
+                      :outbound_type => Message{Categorical},
+                      :inbound_types => (Nothing, Distribution, Distribution),
+                      :name          => VBGFECategoricalOut)
+
+function ruleVBGFECategoricalOut(marg_out::Distribution{Univariate, Categorical}, 
+                                 marg_A::Distribution{MatrixVariate, PointMass},
+                                 marg_c::Distribution{Multivariate, PointMass})
+    s = marg_out.params[:p]
+    A = marg_A.params[:m]
+    c = marg_c.params[:m]
+
+    rho = diag(A'*log.(A .+ tiny)) + A'*log.(c .+ tiny) - A'*log.(A*s .+ tiny)
+
+    Message(Univariate, Categorical, p=rho./sum(rho))
+end
+
+function collectNaiveVariationalNodeInbounds(::GFECategorical, entry::ScheduleEntry)
+    algo = currentInferenceAlgorithm()
+    target_to_marginal_entry = algo.target_to_marginal_entry
+
+    inbounds = Any[]
+    for node_interface in entry.interface.node.interfaces
+        inbound_interface = ultimatePartner(node_interface)
+        if isClamped(inbound_interface)
+            # Hard-code marginal of constant node in schedule
+            push!(inbounds, assembleClamp!(inbound_interface.node, Distribution))
+        else
+            # Collect entry from marginal schedule
+            push!(inbounds, target_to_marginal_entry[node_interface.edge.variable])
         end
     end
 
