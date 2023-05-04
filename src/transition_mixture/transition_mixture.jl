605d458024307fca71dd3dd5a9640b6d2cd01881
magnuskoudahl <magnuskoudahl@gmail.com>
2023-05-04 15:09:04 +0200

    add first draft of marginalrule and average energy

--- src/transition_mixture/transition_mixture.jl
+++ src/transition_mixture/transition_mixture.jl
@@ -1,102 +1,37 @@
-struct TransitionMixture end
+@marginalrule TransitionMixture(:out_in_z) (m_out::DiscreteNonParametric, m_in::DiscreteNonParametric, m_z::DiscreteNonParametric, q_B1::PointMass, q_B2::PointMass, q_B3::PointMass, q_B4::PointMass, ) = begin
 
-@node TransitionMixture Stochastic [out,in,z,B1,B2,B3,B4,]
+    I_ = length(probvec(m_out))
+    J  = length(probvec(m_in))
+    K  = length(probvec(m_z))
+    μ_out = probvec(m_out)
+    μ_in = probvec(m_in)
+    μ_z = probvec(m_z)
 
-# Average energy functional
-#function averageEnergy(::Type{TransitionMixture},
-#                       dist_out_in1_switch::Distribution{Multivariate, Contingency},
-#                       dist_factors::Vararg{Distribution})
-#
-#    n_factors = length(dist_factors)
-#    U = 0.0
-#    for k = 1:n_factors
-#        U += -tr(dist_out_in1_switch.params[:p][k]'*unsafeLogMean(dist_factors[k]))
-#    end
-#
-#    return U
-#end
-
-#@average_eneergy TransitionMixture(blah) = begin
-#    TODO
-#end
-
-
-# m_x means message_x, q_x means marginal x. So we use this to dispatch on SP/VB rules
-@rule TransitionMixture(:out, Marginalisation) (m_in::DiscreteNonParametric,m_z::DiscreteNonParametric,q_B1::PointMass,q_B2::PointMass,q_B3::PointMass,q_B4::PointMass,) = begin
-    z = probvec(m_z)
-
-    # Hacky McHackface
-    B1 = mean(q_B1)
-    B2 = mean(q_B2)
-    B3 = mean(q_B3)
-    B4 = mean(q_B4)
-    Bs = [B1,B2,B3,B4]
-
-    inp = probvec(m_in)
-
-    # Hack some more...
-    p = zeros(size(B1*inp)[1])
-    for k in 1:4
-	p += z[k] * Bs[k] * inp
+    A_tilde = [mean(ReactiveMP.clamplog,q_B1);;; mean(ReactiveMP.clamplog,q_B2);;; mean(ReactiveMP.clamplog,q_B3);;; mean(ReactiveMP.clamplog,q_B4)]
+    B = zeros(I_,J,K)
+    for i in 1:I_
+        for j in 1:J
+            for k in 1:K
+                B[i,j,k] = μ_out[i]*μ_in[j]*μ_z[k] * A_tilde[i,j,k]
+            end
+        end
     end
-
-    return Categorical(p ./ sum(p))
+    return ContingencyTensor(B ./ sum(B))
 end
 
-@rule TransitionMixture(:in, Marginalisation) (m_out::DiscreteNonParametric,m_z::DiscreteNonParametric,q_B1::PointMass,q_B2::PointMass,q_B3::PointMass,q_B4::PointMass,) = begin
-    z = probvec(m_z)
+@average_energy TransitionMixture (q_out_in_z::ContingencyTensor, q_B1::PointMass, q_B2::PointMass, q_B3::PointMass, q_B4::PointMass, meta::PSubstitutionMeta) = begin
 
-    # Hacky McHackface
-    B1 = mean(q_B1)
-    B2 = mean(q_B2)
-    B3 = mean(q_B3)
-    B4 = mean(q_B4)
-    Bs = [B1,B2,B3,B4]
+    log_A_bar = [mean(ReactiveMP.clamplog,q_B1);;; mean(ReactiveMP.clamplog,q_B2);;; mean(ReactiveMP.clamplog,q_B3);;; mean(ReactiveMP.clamplog,q_B4)]
 
-    out = probvec(m_out)
-
-    # This is ugly..
-    p = zeros(size(B1'*out)[1])
-    for k in 1:4
-	p += z[k] * Bs[k]' * out
+    U = 0
+    for i in 1:4
+       U+= -tr(B[:,:,i]' *log_A_bar[:,:,i] )
     end
-
-    return Categorical(p ./ sum(p))
+    U
 end
 
-@rule TransitionMixture(:z, Marginalisation) (m_out::DiscreteNonParametric,m_in::DiscreteNonParametric,q_B1::PointMass,q_B2::PointMass,q_B3::PointMass,q_B4::PointMass,) = begin
-
-    # Hacky McHackface
-    B1 = mean(q_B1)
-    B2 = mean(q_B2)
-    B3 = mean(q_B3)
-    B4 = mean(q_B4)
-    Bs = [B1,B2,B3,B4]
-
-    out = probvec(m_out)
-    inp = probvec(m_in)
-
-    p = zeros(4)
-    for k in 1:4
-	p[k] += out' * Bs[k] * inp
-    end
-
-    return Categorical(p ./ sum(p))
-end
-
-@rule TransitionMixture(:out, Marginalisation) (m_in::DiscreteNonParametric,m_z::PointMass,q_B1::PointMass,q_B2::PointMass,q_B3::PointMass,q_B4::PointMass,) = begin
-
-    z = argmax(m_z.point)
-
-    # Hacky McHackface
-    B1 = mean(q_B1)
-    B2 = mean(q_B2)
-    B3 = mean(q_B3)
-    B4 = mean(q_B4)
-    Bs = [B1,B2,B3,B4]
-
-    p = Bs[z] * probvec(m_in)
-
-    return Categorical(p ./ sum(p))
-end
+include("in.jl")
+include("out.jl")
+include("switch.jl")
+include("marginals.jl")
 
