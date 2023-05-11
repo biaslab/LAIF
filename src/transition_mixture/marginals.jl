4590f79f21bf93bf723f318ee4655003c255f8ad
magnuskoudahl <magnuskoudahl@gmail.com>
2023-05-11 15:01:36 +0200

    Clean up files and remove some hacks

--- src/transition_mixture/marginals.jl
+++ src/transition_mixture/marginals.jl
@@ -15,10 +15,27 @@ Distributions.mean(dist::ContingencyTensor) = dist.p
 
 # Clamplog means
 mean(::typeof(ReactiveMP.clamplog), dist::MatrixDirichlet) = digamma.(ReactiveMP.clamplog.(dist.a)) .- digamma.(sum(ReactiveMP.clamplog.(dist.a)); dims = 1)
-mean(::typeof(ReactiveMP.clamplog), dist::PointMass) = ReactiveMP.clamplog.(mean(dist))
+#mean(::typeof(ReactiveMP.clamplog), dist::PointMass) = ReactiveMP.clamplog.(mean(dist))
 
 
 Distributions.entropy(dist::ContingencyTensor) = -sum(xlogx.(dist.p))
+
+@marginalrule TransitionMixture(:out_in_z) (m_out::DiscreteNonParametric, m_in::DiscreteNonParametric, m_z::DiscreteNonParametric, q_B1::PointMass, q_B2::PointMass, q_B3::PointMass, q_B4::PointMass, ) = begin
+
+    I_ = length(probvec(m_out))
+    J  = length(probvec(m_in))
+    K  = length(probvec(m_z))
+    μ_out = probvec(m_out)
+    μ_in = probvec(m_in)
+    μ_z = probvec(m_z)
+
+    # Need to make this generic
+    A_tilde = [mean(ReactiveMP.clamplog,q_B1);;; mean(ReactiveMP.clamplog,q_B2);;; mean(ReactiveMP.clamplog,q_B3);;; mean(ReactiveMP.clamplog,q_B4)]
+
+    B = cat(map( x -> x * μ_out * μ_in', μ_z)..., dims=3) .* A_tilde
+    return ContingencyTensor(B ./ sum(B))
+end
+
 
 #@marginalrule TransitionMixture(:out_in_z) (m_out::DiscreteNonParametric, m_in::DiscreteNonParametric, m_z::DiscreteNonParametric, q_B1::PointMass, q_B2::PointMass, q_B3::PointMass, q_B4::PointMass, ) = begin
 #
--- src/transition_mixture/transition_mixture.jl
+++ src/transition_mixture/transition_mixture.jl
@@ -1,37 +1,57 @@
-@marginalrule TransitionMixture(:out_in_z) (m_out::DiscreteNonParametric, m_in::DiscreteNonParametric, m_z::DiscreteNonParametric, q_B1::PointMass, q_B2::PointMass, q_B3::PointMass, q_B4::PointMass, ) = begin
+@average_energy TransitionMixture (q_out_in_z::ContingencyTensor, q_B1::PointMass, q_B2::PointMass, q_B3::PointMass, q_B4::PointMass) = begin
 
-    I_ = length(probvec(m_out))
-    J  = length(probvec(m_in))
-    K  = length(probvec(m_z))
-    μ_out = probvec(m_out)
-    μ_in = probvec(m_in)
-    μ_z = probvec(m_z)
+    # Need to make this generic
+    log_A_bar = [mean(ReactiveMP.clamplog,q_B1);;; mean(ReactiveMP.clamplog,q_B2);;; mean(ReactiveMP.clamplog,q_B3);;; mean(ReactiveMP.clamplog,q_B4)]
 
-    A_tilde = [mean(ReactiveMP.clamplog,q_B1);;; mean(ReactiveMP.clamplog,q_B2);;; mean(ReactiveMP.clamplog,q_B3);;; mean(ReactiveMP.clamplog,q_B4)]
-    B = zeros(I_,J,K)
-    for i in 1:I_
-        for j in 1:J
-            for k in 1:K
-                B[i,j,k] = μ_out[i]*μ_in[j]*μ_z[k] * A_tilde[i,j,k]
-            end
-        end
-    end
-    return ContingencyTensor(B ./ sum(B))
+    B = mean(q_out_in_z)
+
+    sum(-tr.(transpose.(eachslice(B,dims=3)) .* eachslice(log_A_bar,dims=3)))
 end
 
-@average_energy TransitionMixture (out_in_z::ContingencyTensor, B1::PointMass, B2::PointMass, B3::PointMass, B4::PointMass) = begin
+#@marginalrule TransitionMixture(:out_in_z) (m_out::DiscreteNonParametric, m_in::DiscreteNonParametric, m_z::DiscreteNonParametric, q_B1::PointMass, q_B2::PointMass, q_B3::PointMass, q_B4::PointMass, ) = begin
+#
+#    I_ = length(probvec(m_out))
+#    J  = length(probvec(m_in))
+#    K  = length(probvec(m_z))
+#    μ_out = probvec(m_out)
+#    μ_in = probvec(m_in)
+#    μ_z = probvec(m_z)
+#
+#    # Need to make this generic
+#    A_tilde = [mean(ReactiveMP.clamplog,q_B1);;; mean(ReactiveMP.clamplog,q_B2);;; mean(ReactiveMP.clamplog,q_B3);;; mean(ReactiveMP.clamplog,q_B4)]
+#
+#    B = cat(map( x -> x * μ_out * μ_in', μ_z)..., dims=3) .* A_tilde
+#    return ContingencyTensor(B ./ sum(B))
+#end
 
-    log_A_bar = [mean(ReactiveMP.clamplog,B1);;; mean(ReactiveMP.clamplog,B2);;; mean(ReactiveMP.clamplog,B3);;; mean(ReactiveMP.clamplog,B4)]
-
-    U = 0
-    for i in 1:4
-       U+= -tr(B[:,:,i]' *log_A_bar[:,:,i] )
-    end
-    return U
-end
-
-#include("in.jl")
-#include("out.jl")
-#include("switch.jl")
-#include("marginals.jl")
-
+#@marginalrule TransitionMixture(:out_in_z) (m_out::DiscreteNonParametric, m_in::DiscreteNonParametric, m_z::DiscreteNonParametric, q_B1::PointMass, q_B2::PointMass, q_B3::PointMass, q_B4::PointMass, ) = begin
+#
+#    I_ = length(probvec(m_out))
+#    J  = length(probvec(m_in))
+#    K  = length(probvec(m_z))
+#    μ_out = probvec(m_out)
+#    μ_in = probvec(m_in)
+#    μ_z = probvec(m_z)
+#
+#    A_tilde = [mean(ReactiveMP.clamplog,q_B1);;; mean(ReactiveMP.clamplog,q_B2);;; mean(ReactiveMP.clamplog,q_B3);;; mean(ReactiveMP.clamplog,q_B4)]
+#    B = zeros(I_,J,K)
+#    for i in 1:I_
+#        for j in 1:J
+#            for k in 1:K
+#                B[i,j,k] = μ_out[i]*μ_in[j]*μ_z[k] * A_tilde[i,j,k]
+#            end
+#        end
+#    end
+#    return ContingencyTensor(B ./ sum(B))
+#end
+#
+#@average_energy TransitionMixture (out_in_z::ContingencyTensor, B1::PointMass, B2::PointMass, B3::PointMass, B4::PointMass) = begin
+#
+#    log_A_bar = [mean(ReactiveMP.clamplog,B1);;; mean(ReactiveMP.clamplog,B2);;; mean(ReactiveMP.clamplog,B3);;; mean(ReactiveMP.clamplog,B4)]
+#
+#    U = 0
+#    for i in 1:4
+#       U+= -tr(B[:,:,i]' *log_A_bar[:,:,i] )
+#    end
+#    return U
+#end
